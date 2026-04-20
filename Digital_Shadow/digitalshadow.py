6d8729e0503cd21701e83ab4de954ae525cfbb4f
SanderSWeme <sander2567@gmail.com>
2026-04-20 17:30:27 +0200

    Updated the Robot function to be part of a class
    Integrated the robot() function to be part of a function renaming it to draw_robot()  under the class of Robot.

--- Digital_Shadow/digitalshadow.py
+++ Digital_Shadow/digitalshadow.py
@@ -15,18 +15,35 @@ pygame.display.set_caption("Digital Shadow")
 WHITE = (255,255,255)
 
 #Simulation Objects
-def robot(start_pos, radius=10, angle=0): # Angle 0 = straight right
-    vector_y = math.sin(angle) * (radius + 2.5)
-    vector_x = math.cos(angle) * (radius + 2.5)
-    end_pos = (start_pos[0]+vector_x, start_pos[1]+vector_y)
+class Robot:
+    def __init__(self, start_pos, radius=10):
+        self.start_pos = start_pos
+        self.radius = radius
+        self.angle = 0
+        
 
-    draw.circle(screen, WHITE, start_pos, radius, width=1)
-    draw.line(screen, WHITE, start_pos, end_pos, width=1)
+    def draw_robot(self, angle=0): # Angle 0 = straight up, Rotates clockwise
+        self.angle = angle
+        angle += 270 # To compensate that makes 0 be up
+        angle_rad = angle * (math.pi / 180) # Converts the degrees to radians
+
+        vector_y = math.sin(angle_rad) * (self.radius + 2.5)
+        vector_x = math.cos(angle_rad) * (self.radius + 2.5)
+        end_pos = (self.start_pos[0]+vector_x, self.start_pos[1]+vector_y)
+
+        draw.circle(screen, WHITE, self.start_pos, self.radius, width=1)
+        draw.line(screen, WHITE, self.start_pos, end_pos, width=1)
  
-
+    def get_robot_facing(): #WIP
+        # Fill with logic to get current facing of robot
+        return
 
 def main():
     run = True
+
+    # Initializing objects
+    robot1 = Robot((300,200)) # Initializing Robot object
+
     while run:
 
         screen.fill((0,0,0))
@@ -37,8 +54,8 @@ def main():
         # create line example
         # draw.line(screen, WHITE, start_pos=(250,200), end_pos=(350, 200), width=1)
 
-        robot((300,200))
-
+        
+        robot1.draw_robot(angle=0)
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
